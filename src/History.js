242089ee5bcddf6f78a94914aea82ae66ebe3a11
Jlab <jeanlabedwcs@gmail.com>
2019-04-09 10:02:14 +0200

    Final commit

---
+++ src/History.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Hystory extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Notre histoire</h1>
+            </div>
+        );
+    }
+}
+
+export default Hystory;
